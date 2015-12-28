c2339a13c99d7edc66ea64004ee8ea50ed190a73
leemark <leemark@gmail.com>
2015-12-27 20:09:33 -0700

    add data as geoJSON

---
+++ data/data.js
@@ -0,0 +1,65 @@
+var poiData = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "id": "1",
+            "properties": {
+                "name": "Seven Minute Springs",
+                "imgSrc": "img/seven_min_springs.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    38.858477, 
+                    -104.911646
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "2",
+            "properties": {
+                "name": "Upper Washington #1",
+                "imgSrc": "img/upper_washington_1.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    38.859022, 
+                    -104.912633
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "3",
+            "properties": {
+                "name": "Upper Washington #2",
+                "imgSrc": "img/upper_washington_2.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    38.858515, 
+                    -104.913594
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "4",
+            "properties": {
+                "name": "Town Clock",
+                "imgSrc": "img/manitou_town_clock.jpg"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    38.857743, 
+                    -104.916374
+                ]
+            }
+        },
+    ]
+};
