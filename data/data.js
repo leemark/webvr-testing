935737bc033c6cd0af56ff0a2adca1d7a801e3e9
leemark <leemark@gmail.com>
2016-01-04 23:46:44 -0700

    added youtube video

--- data/data.js
+++ data/data.js
@@ -5,6 +5,7 @@ var poiData = {
             "type": "Feature",
             "id": "1",
             "properties": {
+                "type": "img",
                 "name": "Seven Minute Springs",
                 "imgSrc": "img/seven_min_springs.jpg"
             },
@@ -20,6 +21,7 @@ var poiData = {
             "type": "Feature",
             "id": "2",
             "properties": {
+                "type": "img",
                 "name": "Upper Washington #1",
                 "imgSrc": "img/upper_washington_1.jpg"
             },
@@ -35,6 +37,7 @@ var poiData = {
             "type": "Feature",
             "id": "3",
             "properties": {
+                "type": "img",
                 "name": "Upper Washington #2",
                 "imgSrc": "img/upper_washington_2.jpg"
             },
@@ -50,6 +53,7 @@ var poiData = {
             "type": "Feature",
             "id": "4",
             "properties": {
+                "type": "img",
                 "name": "Town Clock",
                 "imgSrc": "img/manitou_town_clock.jpg"
             },
@@ -58,6 +62,22 @@ var poiData = {
                 "coordinates": [
                     38.857743, 
                     -104.916374
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "id": "5",
+            "properties": {
+                "type": "vid",
+                "name": "Carnegie Hall",
+                "imgSrc": "https://www.youtube.com/embed/YERHP5wp_zw"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    38.853995, 
+                    -104.920565
                 ]
             }
         },
